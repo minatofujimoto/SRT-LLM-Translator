5007261df0f74eff28bf308a912c2d5bcb08715f
minatofujimoto <minatofujimototrc@gmail.com>
2025-03-04 21:37:58 +0900

    feat: add CLI argument parsing and validation

--- src/__init__.py
+++ src/__init__.py
@@ -7,3 +7,4 @@
 # Unit feature implementation - 20250217_0029
 # Unit feature implementation - 20250228_0046
 # Unit feature implementation - 20250303_0047
+# Unit feature implementation - 20250304_0048
